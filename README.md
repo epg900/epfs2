8c4acd0d03d23a41af5020ab5df982b95a497e0d
epg900 <epg900@gmail.com>
2024-06-09 07:01:37 +0330

    1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+How to use `epfs` file share
+----------------------------
+
+1. clone repository and run `flaskRange.py`.
+2. enjoy it.
---
+++ flaskRange.py
@@ -0,0 +1,45 @@
+from flask import Flask, send_file, redirect,  render_template, request
+import os 
+
+'''
+#For running on android phone uncomment below lines an turn on hotspot on your android phone then start this script on pydroid
+import socket, fcntl, struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+print(f" * Running on http://{get_ip_address(b'wlan0')}:5000")
+'''
+
+abs_path = os.getcwd() #'/storage/emulated/0/Download'
+
+app = Flask(__name__, static_url_path='/static', static_folder = abs_path, template_folder = abs_path)
+
+@app.route('/')
+def index():
+    return redirect('/upload')
+
+@app.route('/upload')
+def dir_listing():
+    files = os.listdir(abs_path + "/upload" )
+    files.sort()
+    return render_template('index.html', files=files)
+
+@app.route('/uploader' , methods = ['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        files = request.files.getlist("file")
+        for f in files:
+            f.save(f'{abs_path}/upload/{f.filename}')
+        return redirect('/upload')
+
+@app.route('/p/<filename>')
+def play(filename):
+	return send_file(f'{abs_path}/upload/{filename}', conditional = True)
+
+app.run(host="0.0.0.0")
